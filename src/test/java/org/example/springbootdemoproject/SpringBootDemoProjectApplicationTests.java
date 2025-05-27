e72d2c93090431100d3d53634d353822e8436caf
2069530 <2069530@ppdz.pupumall.com>
2025-05-27 14:27:04 +0800

    pom

---
+++ src/test/java/org/example/springbootdemoproject/SpringBootDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
